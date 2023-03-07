e2bab9b9ab0cfc1af48c57e5e976e3f3cb8bd060
quangtuan9997 <mrt.tuan97@gmail.com>
2023-03-07 10:32:43 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.printf("Nhập tên mày vào:");
+        String name= sc.nextLine();
+        System.out.printf("Xin chào"+name);
+    }
+}
